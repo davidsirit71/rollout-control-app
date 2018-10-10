084467103becfad548b583fbc0a1d4cebd8a7c66
David Sirit <davidsirit@yahoo.com>
2018-10-10 18:36:30 +0200

    auth route tested with postman

--- server/app.js
+++ server/app.js
@@ -1,85 +1,126 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
-const session    = require("express-session");
-const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
-    
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+//const flash = require("connect-flash");
+
+const cors = require("cors");
+const passport = require("passport"); // not sure
+
+const { DBURL } = process.env;
+mongoose.Promise = Promise; // ???
 
 mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
+  .connect(
+    DBURL,
+    { useNewUrlParser: true }
+  )
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+let whitelist = [
+  'http://localhost:3000'
+];
+let corsOptions = {
+  origin: function(origin, callback){
+      let originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+      callback(null, originIsWhitelisted);
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
+
+// Middleware Setup
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
-hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
-  } else {
-      return options.fn(this);
-  }
-});
-  
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-// Enable authentication using session + passport
 app.use(session({
-  secret: 'irongenerator',
+  secret: 'angular auth passport secret shh',
   resave: true,
   saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
-}))
-app.use(flash());
+  cookie:{
+    httpOnly: true,
+    maxAge: 2419200000
+  },
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 require('./passport')(app);
-    
 
-const index = require('./routes/index');
-app.use('/', index);
 
-const authRoutes = require('./routes/auth');
-app.use('/auth', authRoutes);
-      
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+
+// hbs.registerHelper("ifUndefined", (value, options) => {
+//   if (arguments.length < 2)
+//     throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
+//   if (typeof value !== undefined) {
+//     return options.inverse(this);
+//   } else {
+//     return options.fn(this);
+//   }
+// });
+
+// default value for title local
+app.locals.title = "Express - Generated with IronGenerator";
+
+// Enable authentication using session + passport
+// app.use(
+//   session({
+//     secret: "irongenerator",
+//     resave: true,
+//     saveUninitialized: true,
+//     store: new MongoStore({ mongooseConnection: mongoose.connection })
+//   })
+// );
+// app.use(flash());
+// require("./passport")(app);
+
+const index = require("./routes/index");
+app.use("/", index);
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
+//const genericCrud = require('./routes/genericCRUD');
+//app.use('/api/user', genericCrud(require('./models/User')));
+//app.use('/api/project', genericCrud(require('./models/Project')));
+//app.use('/api/outsourcing', genericCrud(require('./models/Outsourcing')));
+//app.use('/api/sitelocation', genericCrud(require('./models/SiteLocation')));
 
 module.exports = app;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,63 +1,107 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
-const bcryptSalt = 10;
+//const bcryptSalt = 10;
 
+const login = (req, user) => {
+  return new Promise((resolve, reject) => {
+    req.login(user, err => {
+      console.log("req.login");
+      console.log(user);
+      if (err) {
+        reject(new Error("Something went wrong"));
+      } else {
+        resolve(user);
+      }
+    });
+  });
+};
 
-router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
-});
-
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
-
+// SIGNUP and LOGGED USER
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
 router.post("/signup", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
-    return;
+  console.log(" Hi David entraste");
+  const { username, password, email, roll, image } = req.body;
+  console.log("username", username);
+  console.log("password", password);
+  console.log("email", email);
+  console.log("roll", roll);
+  console.log("imgurl", image);
+
+  // Check for non empty required fields
+  if (username === "" || password === "" || email === "" || roll === "") {
+    next(new Error("You must provide valid credentials"));
   }
 
-  User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
-      return;
-    }
+  // Check if user exists in DB
+  User.findOne({ username })
+    .then(foundUser => {
+      if (foundUser) throw new Error("Username already exists");
 
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
+      const salt = bcrypt.genSaltSync(10);
+      const hashPass = bcrypt.hashSync(password, salt);
 
-    const newUser = new User({
-      username,
-      password: hashPass
-    });
-
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
+      return new User({
+        username,
+        password: hashPass,
+        email,
+        roll,
+        image
+      }).save();
     })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
-  });
+    .then(savedUser => login(req, savedUser)) // loging new user using passport
+    .then(user => res.json({ status: "signup & loging succesfully", user })) // jason received
+    .catch(e => next(e));
 });
 
+// LOGING USER
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { message: req.flash("error") });
+});
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, theUser, failureDetails) => {
+    //error checking
+    if (err) next(new Error("Something went wrong"));
+    if (!theUser) next(failureDetails);
+
+    //Return user and logged in
+    login(req, theUser).then(user => res.status(200).json(req.user));
+  })(req, res, next);
+});
+
+//GET Current user
+router.get("/currentuser", (req, res, next) => {
+  if (req.user) {
+    res.status(200).json(req.user);
+  } else {
+    next(new Error("Not logged in"));
+  }
+});
+
+// router.post("/login", passport.authenticate("local", {
+//   successRedirect: "/",
+//   failureRedirect: "/auth/login",
+//   failureFlash: true,
+//   passReqToCallback: true
+// }));
+
+// LOGING USER
 router.get("/logout", (req, res) => {
   req.logout();
-  res.redirect("/");
+  res.status(200).json({ massage: "logged out" });
+  //res.redirect("/");
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({ massage: err.message });
 });
 
 module.exports = router;
